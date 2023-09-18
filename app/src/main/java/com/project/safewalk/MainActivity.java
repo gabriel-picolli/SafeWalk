03253d787eb90db24a5dcec35f26647e6e02a73f
gabriel-picolli <gabriel_picollit@hotmail.com>
2023-09-18 17:32:21 -0300

    Plot map API Google Maps

--- app/src/main/java/com/project/safewalk/MainActivity.java
+++ app/src/main/java/com/project/safewalk/MainActivity.java
@@ -1,16 +1,79 @@
 package com.project.safewalk;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final int PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+
+        // Verificar permissão de localização
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Enable the location layer if the permission is granted
+            googleMap.setMyLocationEnabled(true);
+            showMyLocation();
+        } else {
+            // Solicitar permissão de localização
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_ACCESS_FINE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permissão concedida, habilite a camada de localização.
+                if(googleMap != null) {
+                    googleMap.setMyLocationEnabled(true);
+                    showMyLocation();
+                }
+            }
+        }
+    }
+
+    private void showMyLocation() {
+        // Obter a última localização conhecida
+        Location location = googleMap.getMyLocation();
+
+        if (location != null) {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            LatLng myLocation = new LatLng(latitude, longitude);
+            googleMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+            googleMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
+        }
     }
 }
